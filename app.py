e4a02bfd76ea2472c560a812805219d2c169a95d
NullWhiteCode <radattheinternet@gmail.com>
2026-07-05 22:56:21 +0100

    Add folder path form

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return """
-    <h1>MemeBrain</h1>
-
-    <p>Hello teammate 😎</p>
-    """
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    folder_path = None
+    if request.method == 'POST':
+        folder_path = request.form.get('folder_path')
+    return render_template('index.html', folder_path=folder_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
