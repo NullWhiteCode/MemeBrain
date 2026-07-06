c952e928d1588f2d775147ce2ccacecc979f5523
NullWhiteCode <radattheinternet@gmail.com>
2026-07-06 21:45:35 +0100

    Display directory scan results

--- app.py
+++ app.py
@@ -6,16 +6,21 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     folder_path = None
+
+    files = []
+    directories = []
+
     if request.method == 'POST':
         folder_path = request.form.get('folder_path')
         folder_path = Path(folder_path)
         if folder_path.is_dir():
             for child in folder_path.iterdir():
                 if child.is_file():
-                    print(f"File: {child.name}")
+                    files.append(child.name)
                 elif child.is_dir():
-                    print(f"Directory: {child.name}")
-    return render_template('index.html', folder_path=folder_path)
+                    directories.append(child.name)
+
+    return render_template('index.html', folder_path=folder_path, files=files, directories=directories)
 
 if __name__ == '__main__':
     app.run(debug=True)
