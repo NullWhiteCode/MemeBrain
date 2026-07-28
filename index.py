7d59cc948347dc4249082cf842f8a1a633a8cbe7
NullWhiteCode <radattheinternet@gmail.com>
2026-07-28 23:03:21 +0100

    Add idempotent library indexing to SQLite

--- index.py
+++ index.py
@@ -1,19 +1,79 @@
 import sqlite3, time
+from pathlib import Path
+
+from library import index_library
+
 
 def setupDatabase():
     con = sqlite3.connect("database/memebrain.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS files(id INTEGER PRIMARY KEY, path TEXT, modified_time REAL,  file_size INTEGER, indexed_time REAL, status TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS files(id INTEGER PRIMARY KEY, path TEXT UNIQUE, modified_time REAL, file_size INTEGER, indexed_time REAL, status TEXT)")
 
     con.commit()
     con.close()
 
 
+def fileLookup(path):
+    con = sqlite3.connect("database/memebrain.db")
+    cur = con.cursor()
+    pathString = (str(path),)
 
+    cur.execute("SELECT * FROM files WHERE path = ?", 
+                pathString,
+                )
+
+    result = cur.fetchone()
+    con.close()
+
+    return result
+
+
+def insertFile(path):
+    con = sqlite3.connect("database/memebrain.db")
+    cur = con.cursor()
+
+    stats = path.stat()
+    modified_time = stats.st_mtime
+    file_size = stats.st_size
+    file_path = str(path)
+    indexed_time = time.time()
+    status = "indexed"
+
+    cur.execute("""
+    INSERT INTO files
+        (path, modified_time, file_size, indexed_time, status)
+        VALUES (?, ?, ?, ?, ?)
+        """, (
+            file_path,
+            modified_time,
+            file_size,
+            indexed_time,
+            status,
+        )
+        )
+
+    con.commit()
+    con.close()
+
+
+def store_library_index(library_index):
+    for item in library_index:
+        path = item["path"]
+
+        if fileLookup(path) is None:
+            insertFile(path)
+
+
+def index_folder(library_path):
+    library_index = index_library(library_path)
+    store_library_index(library_index)
 
 
 if __name__ == "__main__":
     setupDatabase()
+
+    library = Path(r"E:\Null\Pictures\Spicy Memes")
+    index_folder(library)
 
 
 
